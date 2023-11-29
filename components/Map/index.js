2a1b4db879cda4e04b41d020cbcb2e9b79da8488
Peter Anderson <pete.anderson@gmail.com>
2023-11-29 11:28:54 +0100

    Add markers

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,6 +1,8 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
 
 const StyledMapContainer = styled(MapContainer)`
   height: 30rem;
@@ -8,13 +10,22 @@ const StyledMapContainer = styled(MapContainer)`
   margin: 0 auto;
 `;
 
-export default function Map() {
+export default function Map({ markers }) {
   return (
     <StyledMapContainer center={[53.586, 7.133]} zoom={12} scrollWheelZoom>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {markers.map((marker) => (
+        <Marker key={marker.id} position={[marker.lat, marker.long]}>
+          <Popup>
+            <h2>{marker.name}</h2>
+            <p>This is a really good description.</p>
+          </Popup>
+        </Marker>
+      ))}
     </StyledMapContainer>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,32 @@ import dynamic from "next/dynamic";
 
 const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
+const markers = [
+  {
+    id: "7c5qktU8",
+    name: "Roßmarkt Frankfurt",
+    lat: 50.1123904,
+    long: 8.6753527,
+  },
+  {
+    id: "2F11t82g",
+    name: "Currywurst Wagen Hamburg",
+    lat: 53.5450717,
+    long: 10.0267705,
+  },
+  {
+    id: "2F11fsdsfdg",
+    name: "Schloss Lütetsburg",
+    lat: 53.599779738815066,
+    long: 7.263257092625226,
+  },
+];
+
 export default function HomePage() {
   return (
     <div>
       <h1>Leaflet demo</h1>
-      <Map />
+      <Map markers={markers} />
     </div>
   );
 }
